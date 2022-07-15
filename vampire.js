76e3937388e33e5bc9e98385fd0cdd10db96f029
Philip Simpson <pwsjas433@hotmail.com>
2022-07-15 18:11:59 +0000

    implement basic functions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let count = 0; //Count Dracula?
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      count++;
+      currentVampire = currentVampire.creator;
+    }
+    
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,7 +48,52 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    const thisArray = [];
+    const vampArray = [];
+    let currentVampire = this;
 
+    if (this === vampire) {
+      return this;
+    }
+
+    if (this.creator === null) {
+      return this;
+    }
+
+    if (vampire.creator === null) {
+      return vampire;
+    }
+
+    if (this.creator === vampire.creator) {
+      return vampire.creator;
+    }
+
+    while (currentVampire.creator) {
+      thisArray.push(currentVampire);
+      currentVampire = currentVampire.creator;
+    }
+
+    currentVampire = vampire;
+    while (currentVampire.creator) {
+      vampArray.push(currentVampire);
+      currentVampire = currentVampire.creator;
+    }
+
+    for (const vamp of thisArray) {
+      if (vampArray.includes(vamp)) {
+        return vamp;
+      }
+    }
+
+    if (thisArray.includes(vampire)) {
+      return vampire.name;
+    }
+
+    if (vampArray.includes(this)) {
+      return this.name;
+    }
+
+    return thisArray[thisArray.length - 1].creator;
   }
 }
 
